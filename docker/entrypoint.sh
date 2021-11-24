7b2bc8813967d97cddf8b0436836fe21b0f07c92
Sven Fischer <git-dev@linux4tw.de>
2021-11-24 23:41:25 +0100

    use local osmbc in container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+cd osmbc
 
-if [ -f init-db.flag ]; then
+npm install
+
+dbflag="../init-db.flag"
+if [ -f $dbflag ]; then
    echo "First start, creating database."
-   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose && rm init-db.flag
+   node ./import/createdb.js --createTables --dropTables --createView --createIndex --addUser $OSMUSERNAME --verbose && rm $dbflag
 fi
 
 # startup
